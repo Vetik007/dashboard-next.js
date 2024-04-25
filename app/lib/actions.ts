2dd33c1d0244985d9d0bcea85427a85b8cc4bb76
Vetik007 <boy0007@ukr.net>
2024-04-25 22:31:53 +0300

    update invoices: add createinvoice, add updateinvoice,  add delete invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,13 +1,80 @@
 'use server'
 
+import { z } from 'zod'
+import { sql } from '@vercel/postgres'
+import { revalidatePath } from 'next/cache'
+import {redirect} from 'next/navigation'
+
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(),
+    status: z.enum(['pending', 'paid']),
+    date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true })
+const UpdateInvoice = FormSchema.omit({ id: true, date: true })
+
+// создание нового счета
 export async function createInvoice(formData: FormData) {
-    const rawFormData = {
+
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-    };
+    });
 
-    console.log('rawFormData:', rawFormData)
+    const amountInCents = amount * 100; // переводим денежное значение в центы
+    const date = new Date().toISOString().split('T')[0]; // создаем новую дату в формате «ГГГГ-ММ-ДД»
+     
+    // SQL-запрос, чтобы вставить новый счет в базу данных и передать переменные
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `
+
+    revalidatePath('/dashboard/invoices'); //очищаем кеш браузера и инициируем новый запрос к серверу.После обновления базы данных путь (/dashboard/invoices) будет повторно проверен, и с сервера будут получены свежие данные.
+        
+    redirect('/dashboard/invoices'); //  после создания нового счета перенаправляем пользователя обратно на страницу (/dashboard/invoices)
 
 
+
+    // const rawFormData = {
+    //     customerId: formData.get('customerId'),
+    //     amount: formData.get('amount'),
+    //     status: formData.get('status'),
+    // };
+
+    // console.log('rawFormData:', rawFormData)
+
+    // console.log('rawFormData1:', typeof rawFormData.amount)
+
+
+}
+
+// обновление счета
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`
+    revalidatePath('/dashboard/invoices');
 }
